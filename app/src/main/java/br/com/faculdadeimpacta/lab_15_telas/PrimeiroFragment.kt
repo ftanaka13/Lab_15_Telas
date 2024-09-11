b4c13a7716254a5fc76dc0554d8f5d667da58dd5
Fernando Tanaka <fernando.tanaka@outlook.com>
2024-09-10 22:53:36 -0300

    codigo PrimeiroFragment

--- app/src/main/java/br/com/faculdadeimpacta/lab_15_telas/PrimeiroFragment.kt
+++ app/src/main/java/br/com/faculdadeimpacta/lab_15_telas/PrimeiroFragment.kt
@@ -5,55 +5,44 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import br.com.faculdadeimpacta.lab_15_telas.databinding.FragmentPrimeiroBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [PrimeiroFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PrimeiroFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentPrimeiroBinding? = null
+    private val binding get() = _binding!!
+    private val args: PrimeiroFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_primeiro, container, false)
+        _binding = FragmentPrimeiroBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PrimeiroFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PrimeiroFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onStart() {
+        super.onStart()
+
+        binding.textViewValor.text = if (args.valor != 0) {
+            "${args.valor}"
+        } else {
+            ""
+        }
+
+        binding.buttonTipo1.setOnClickListener {
+            val directions =
+                PrimeiroFragmentDirections.actionPrimeiroFragmentToSegundoFragment(10, 1)
+            findNavController().navigate(directions)
+        }
+
+        binding.buttonTipo2.setOnClickListener {
+            val directions =
+                PrimeiroFragmentDirections.actionPrimeiroFragmentToSegundoFragment(20, 2)
+            findNavController().navigate(directions)
+        }
     }
 }
